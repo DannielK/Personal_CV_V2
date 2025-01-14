942c552f60f647be3a9d9196efca094a1d2e3f92
DannielK <dannykm003@gmail.com>
2025-01-13 21:27:04 -0800

    Husky test

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 const Sidebar = () => {
     return (
         <aside className="sticky top-0 flex max-h-screen w-[48%] flex-col justify-between py-24 border-2 border-red-500">
+            TEST
         </aside>
     );
 };
