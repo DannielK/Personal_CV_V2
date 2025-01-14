9ff27b3f0bf410a5d68fdd00fd6ff2244c5974b6
Danniel Kim <102492968+DannielK@users.noreply.github.com>
2025-01-14 00:23:32 -0800

    General site layout completed (#21)
    General Site Layout Completed, Workflow Fixed, Husky Fixed

---
+++ src/components/Content.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+
+const Content = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="w-[52%] border-2 border-blue-500 py-24 pt-24">
+      {children}
+      TEST
+    </main>
+  );
+};
+
+export default Content;
