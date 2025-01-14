fc6b6458f80719b1a7a82a23dbe38bf1c6031b86
DannielK <dannykm003@gmail.com>
2025-01-13 20:02:06 -0800

    Added husky and lint-staged

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
