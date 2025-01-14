7cdd3c3776e1768be7d7cc09b60935d52e2d7392
DannielK <dannykm003@gmail.com>
2025-01-13 22:21:26 -0800

    maybe again?

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 
-const Content = ({children}: {   children: ReactNode}) => {
+const Content = ({children}: {   children: ReactNode        }) => {
   return (
     <main className="w-[52%] border-2 border-blue-500 py-24 pt-24">
       {children}
