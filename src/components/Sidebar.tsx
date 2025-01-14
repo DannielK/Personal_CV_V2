c6a54f1335168aa0b3945efc3fcfa733236c12d6
DannielK <dannykm003@gmail.com>
2025-01-13 21:17:02 -0800

    General site layout completed

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+const Sidebar = () => {
+    return (
+        <aside className="sticky top-0 flex max-h-screen w-[48%] flex-col justify-between py-24 border-2 border-red-500">
+        </aside>
+    );
+};
+
+export default Sidebar;
