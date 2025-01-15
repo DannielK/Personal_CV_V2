303ba4b23a21b99accb22ac1fb313f18aff69069
DannielK <dannykm003@gmail.com>
2025-01-15 00:21:43 -0800

    Social links completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <section id="about" className="h-screen w-1/2 border-2 border-red-500">
+        ABOUT
+      </section>
+      <section
+        id="experience"
+        className="h-screen w-1/2 border-2 border-green-500"
+      >
+        EXPERIENCE
+      </section>
+      <section
+        id="projects"
+        className="h-screen w-1/2 border-2 border-blue-500"
+      >
+        PROJECTS
+      </section>
+    </div>
+  );
 }
