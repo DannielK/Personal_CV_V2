fbc37a2037f89d64b21d50056619cdeae1c6096b
DannielK <dannykm003@gmail.com>
2025-02-13 01:43:35 -0800

    Setup for CloudFlare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,5 +11,11 @@ const nextConfig: NextConfig = {
   basePath: "",
   assetPrefix: "",
 };
+
+(async () => {
+  if (process.env.NODE_ENV === "development") {
+    await setupDevPlatform();
+  }
+})();
 
 export default nextConfig;
